ce34a8256a89b6674a9d156edc1767d2a613e1fe
Pjotr <pjotrzwartenkot@gmail.com>
2025-07-30 21:59:50 +0200

    1.6: unicafe step 1

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+const App = () => {
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => setGood(good + 1)
+  const handleNeutral = () => setNeutral(neutral + 1)
+  const handleBad = () => setBad(bad + 1)
+  
+  return (
+    <>
+      <h1>give feedback</h1>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </>
+  )
+}
+
+export default App
