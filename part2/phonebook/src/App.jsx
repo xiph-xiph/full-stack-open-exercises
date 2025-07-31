0112b1b99ddee885fa987d169071a3dacacad2a6
Pjotr <pjotrzwartenkot@gmail.com>
2025-07-31 18:57:48 +0200

    2.13: The Phonebook step 8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/persons'
 import NumberForm from './components/NumberForm'
 import Filter from './components/Filter'
 import PersonList from './components/PersonList'
@@ -12,8 +12,8 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
+    personService
+      .getAll()
       .then(response => {
         setPersons(response.data)
       })
@@ -52,8 +52,8 @@ const App = () => {
       number: newNumber
     }
 
-    axios
-      .post('http://localhost:3001/persons', newPerson)
+    personService
+      .create(newPerson)
       .then((response) => {
         setPersons(persons.concat(response.data))
         setNewName('')
