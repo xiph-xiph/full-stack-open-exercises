672e67920a85912252995b03fb2bffcff5b8d2ec
Pjotr <pjotrzwartenkot@gmail.com>
2025-08-01 22:09:57 +0200

    3.4: Phonebook backend step 4

--- part3/phonebook_backend/src/main.js
+++ part3/phonebook_backend/src/main.js
@@ -2,6 +2,8 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json())
+
 const persons = [
   {
     "id": "1",
@@ -34,12 +36,22 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const foundPerson = persons.find((note) => note.id === request.params.id)
-  if (foundPerson) {
-    response.json(foundPerson)
-  } else {
+  const foundPerson = persons.find((person) => person.id === request.params.id)
+  if (!foundPerson) {
     response.status(404).end()
+    return
   }
+  response.json(foundPerson)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const indexToDelete = persons.findIndex((person) => person.id === request.params.id)
+  if (indexToDelete === -1) {
+    response.status(404).end()
+    return
+  }
+  persons.splice(indexToDelete, 1)
+  response.status(204).end()
 })
 
 const PORT = 3001
