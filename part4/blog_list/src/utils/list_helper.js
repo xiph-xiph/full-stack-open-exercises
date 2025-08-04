c105a09539ac3dacd3d37bfcf1ba770a75838d3d
Pjotr <pjotrzwartenkot@gmail.com>
2025-08-04 22:48:27 +0200

    4.4: Helper Functions and Unit Tests, step 2

--- part4/blog_list/src/utils/list_helper.js
+++ part4/blog_list/src/utils/list_helper.js
@@ -3,6 +3,12 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((previous, current) => (previous + current.likes), 0)
+}
+
+
 export default {
-  dummy
+  dummy,
+  totalLikes
 }
--- part4/blog_list/tests/list_helper.test.js
+++ part4/blog_list/tests/list_helper.test.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 import { test, describe } from 'node:test'
 import assert from 'node:assert'
 import listHelper from '../src/utils/list_helper.js'
@@ -8,4 +7,74 @@ test('dummy returns one', () => {
 
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+  const blogs = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: '5a422b891b54a676234d17fa',
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: '5a422ba71b54a676234d17fb',
+      title: 'TDD harms architecture',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: '5a422bc61b54a676234d17fc',
+      title: 'Type wars',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+      likes: 2,
+      __v: 0
+    }
+  ]
+  const listWithOneBlog = [blogs[0]]
+
+  const bunchOfUnlikedBlogs = [blogs[4], blogs[4], blogs[4], blogs[4], blogs[4], blogs[4], blogs[4], blogs[4]]
+
+  test('of a bigger list should be calculated right', () => {
+    const result = listHelper.totalLikes(blogs)
+    assert.strictEqual(result, 36)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 7)
+  })
+  test('of a list of blogs with 0 likes should be 0', () => {
+    const result = listHelper.totalLikes(bunchOfUnlikedBlogs)
+    assert.strictEqual(result, 0)
+  })
 })
