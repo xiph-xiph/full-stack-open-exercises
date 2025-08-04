581ba8892a6d4e78c0e78fad031e95657217a40f
Pjotr <pjotrzwartenkot@gmail.com>
2025-08-04 15:31:44 +0200

    3.20*: Phonebook database, step 8

--- part3/phonebook_backend/src/models/person.js
+++ part3/phonebook_backend/src/models/person.js
@@ -14,9 +14,34 @@ mongoose.connect(url)
     process.exit(1)
   })
 
+const isValidNumber = (number) => {
+  // todo: change to only regex
+  if (/[^\d-]/.test(number)) {
+    return false
+  }
+  if (number.length < 8) {
+    return false
+  }
+  if (number.split('-').length !== 2) {
+    return false
+  }
+  if (!(number[2] === '-') && !(number[3] === '-')) {
+    return false
+  }
+  return true
+}
+
 const personSchema = new mongoose.Schema({
-  name: { type: String, required: true, minLength: 3},
-  number: { type: String, required: true }
+  name: {
+    type: String,
+    required: true,
+    minLength: 3},
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: isValidNumber,
+      message: props => `${props.value} is not a valid phone number.`}}
 })
 
 personSchema.set('toJSON', {
