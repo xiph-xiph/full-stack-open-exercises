5fca99446112fb50c67cec336e3ac71eb142f234
Pjotr <pjotrzwartenkot@gmail.com>
2025-08-12 20:06:04 +0200

    5.7 Blog List Frontend, step 7

--- part5/blog_list_frontend/src/components/Blog.jsx
+++ part5/blog_list_frontend/src/components/Blog.jsx
@@ -1,7 +1,32 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+import Toggleable from "./Toggleable"
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [isVisible, setIsVisible] = useState(false)
+
+  const toggleVisibility = () => setIsVisible(!isVisible)
+
+  return (
+    <div style={ blogStyle }>
+      { blog.title } { blog.author } 
+      <button onClick={ toggleVisibility }>{ isVisible ? 'Hide' : 'Show' }</button>
+      { isVisible && (
+        <>
+          <div>{ blog.url }</div>
+          <div>{ 'likes: ' + blog.likes }</div>
+          <div>{ blog.user.name }</div>
+        </>
+      )}
+    </div>
+  )
+}
 
 export default Blog
