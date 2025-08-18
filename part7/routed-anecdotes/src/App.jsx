4f3fac85cb91437742d58ae76844d6184b471f74
Pjotr <pjotrzwartenkot@gmail.com>
2025-08-18 12:41:06 +0200

    7.5: Anecdotes and Hooks step 2

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -84,6 +84,12 @@ const CreateNew = (props) => {
     navigate('/')
   }
 
+  const handleReset = () => {
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -100,7 +106,8 @@ const CreateNew = (props) => {
           url for more info
           <input { ...info } />
         </div>
-        <button>create</button>
+        <button type='submit' >create</button>
+        <button type='button' onClick={ handleReset } >reset</button>
       </form>
     </div>
   )
